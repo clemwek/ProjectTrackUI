e50113b0b27d0ac370f8c1a06bb0b15ef18fc34c
Clement <clemwek@gmail.com>
2025-05-04 19:11:57 +0300

    Initial Commit

---
+++ ProjectTrackUI/ProjectTrackUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectTrackUIApp.swift
+//  ProjectTrackUI
+//
+//  Created by Clement  Wekesa on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjectTrackUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
